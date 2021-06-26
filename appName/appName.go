d6655621ac7c125a3f82353a3f3a88b0675d4e3b
金 <>
2021-06-26 14:16:22 +0800

    appName 懒加载

--- appName/appName.go
+++ appName/appName.go
@@ -4,12 +4,18 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sync"
 )
 
+var appName string
+var once sync.Once
+
 func Get() string {
-	appName, _ := exec.LookPath(os.Args[0])
-	ext := filepath.Ext(appName)
-	appName = filepath.Base(appName)
-	appName = appName[:len(appName)-len(ext)]
+	once.Do(func() {
+		appName, _ = exec.LookPath(os.Args[0])
+		ext := filepath.Ext(appName)
+		appName = filepath.Base(appName)
+		appName = appName[:len(appName)-len(ext)]
+	})
 	return appName
 }
