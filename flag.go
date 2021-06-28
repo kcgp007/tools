0336c0d8c3970fbdb715758d7de8e6d3364e963a
金 <>
2021-06-28 12:06:11 +0800

    添加编译注释

--- flag.go
+++ flag.go
@@ -10,7 +10,7 @@ import (
 var help = pflag.BoolP("help", "h", false, "帮助")
 var v = pflag.BoolP("version", "v", false, "版本")
 
-// -ldflags "-X tools.version=v1.2.3"
+// CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -trimpath -ldflags "-X main.version=v1.2.3"
 var version string
 
 // 帮助&版本
