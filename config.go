fbccc74c3e53799239a2be778c960e490ec2405c
金 <>
2021-07-13 15:50:52 +0800

    取消补全

--- config.go
+++ config.go
@@ -7,7 +7,6 @@ import (
 	"reflect"
 	"strconv"
 	"strings"
-	"time"
 	"unicode"
 
 	"github.com/fsnotify/fsnotify"
@@ -27,12 +26,15 @@ var Log log
 
 var configPath = pflag.StringP("configPath", "c", ".", "配置文件路径")
 
+var isWrite = false
+
 func init() {
 	pflag.Parse()
 	viper.SetConfigName("config")
 	viper.AddConfigPath(*configPath)
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println(err)
+		isWrite = true
 		viper.WriteConfigAs(*configPath + "/config.yml")
 	}
 	viper.WatchConfig()
@@ -47,14 +49,58 @@ func init() {
 // 添加配置
 func AddConfig(p ...interface{}) {
 	for _, p := range p {
+		if isWrite {
+			setDefault(p)
+			viper.WriteConfig()
+		}
 		read(p)
 	}
 	ps = append(ps, p...)
 }
 
+// 设置配置默认值
+func setDefault(p interface{}) {
+	v := reflect.ValueOf(p).Elem()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		typeField := v.Type().Field(i)
+		switch typeField.Type.Kind() {
+		case reflect.String:
+			viper.SetDefault(genKey(v.Type().Name(), typeField.Name), typeField.Tag.Get("default"))
+		case reflect.Int:
+			i, _ := strconv.Atoi(typeField.Tag.Get("default"))
+			viper.SetDefault(genKey(v.Type().Name(), typeField.Name), i)
+		case reflect.Float64:
+			f, _ := strconv.ParseFloat(typeField.Tag.Get("default"), 64)
+			viper.SetDefault(genKey(v.Type().Name(), typeField.Name), f)
+		case reflect.Bool:
+			b, _ := strconv.ParseBool(typeField.Tag.Get("default"))
+			viper.SetDefault(genKey(v.Type().Name(), typeField.Name), b)
+		case reflect.Slice:
+			switch reflect.New(field.Type().Elem()).Elem().Type().Kind() {
+			case reflect.String:
+				ss := strings.Split(typeField.Tag.Get("default"), ",")
+				viper.SetDefault(genKey(v.Type().Name(), typeField.Name), ss)
+			case reflect.Int:
+				ss := strings.Split(typeField.Tag.Get("default"), ",")
+				is := make([]int, len(ss))
+				for i, s := range ss {
+					is[i], _ = strconv.Atoi(s)
+				}
+				viper.SetDefault(genKey(v.Type().Name(), typeField.Name), is)
+			}
+		default:
+			switch typeField.Type.String() {
+			case "time.Duration":
+				i, _ := strconv.Atoi(typeField.Tag.Get("default"))
+				viper.SetDefault(genKey(v.Type().Name(), typeField.Name), i)
+			}
+		}
+	}
+}
+
 // 读取配置，补充缺少的配置
 func read(p interface{}) {
-	isWrite := false
 	v := reflect.ValueOf(p).Elem()
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
@@ -62,76 +108,29 @@ func read(p interface{}) {
 		key := genKey(v.Type().Name(), typeField.Name)
 		switch typeField.Type.Kind() {
 		case reflect.String:
-			if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-				isWrite = true
-				viper.SetDefault(key, typeField.Tag.Get("default"))
-			}
+
 			if strings.Contains(strings.ToLower(typeField.Name), "dir") {
 				createDir(viper.GetString(key))
 			}
 			field.SetString(viper.GetString(key))
 		case reflect.Int:
-			if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-				isWrite = true
-				i, _ := strconv.Atoi(typeField.Tag.Get("default"))
-				viper.SetDefault(key, i)
-			}
 			field.SetInt(viper.GetInt64(key))
 		case reflect.Float64:
-			if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-				isWrite = true
-				f, _ := strconv.ParseFloat(typeField.Tag.Get("default"), 64)
-				viper.SetDefault(key, f)
-			}
 			field.SetFloat(viper.GetFloat64(key))
 		case reflect.Bool:
-			if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-				isWrite = true
-				b, _ := strconv.ParseBool(typeField.Tag.Get("default"))
-				viper.SetDefault(key, b)
-			}
 			field.SetBool(viper.GetBool(key))
 		case reflect.Slice:
 			switch reflect.New(field.Type().Elem()).Elem().Type().Kind() {
 			case reflect.String:
-				if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-					isWrite = true
-					ss := strings.Split(typeField.Tag.Get("default"), ",")
-					viper.SetDefault(key, ss)
-				}
 				field.Set(reflect.ValueOf(viper.GetStringSlice(key)))
 			case reflect.Int:
-				if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-					isWrite = true
-					ss := strings.Split(typeField.Tag.Get("default"), ",")
-					is := make([]int, len(ss))
-					for i, s := range ss {
-						is[i], _ = strconv.Atoi(s)
-					}
-					viper.SetDefault(key, is)
-				}
 				field.Set(reflect.ValueOf(viper.GetIntSlice(key)))
 			}
 		default:
 			switch typeField.Type.String() {
 			case "time.Duration":
-				if !viper.InConfig(cc2sc(v.Type().Name())) || !viper.Sub(cc2sc(v.Type().Name())).InConfig(cc2sc(typeField.Name)) {
-					isWrite = true
-					i, _ := strconv.Atoi(typeField.Tag.Get("default"))
-					viper.SetDefault(key, i)
-				}
 				field.Set(reflect.ValueOf(viper.GetDuration(key)))
 			}
-		}
-	}
-	if isWrite {
-	rewrite:
-		i := 1
-		err := viper.WriteConfig()
-		if err != nil {
-			time.Sleep(time.Duration(i) * time.Second)
-			i *= 2
-			goto rewrite
 		}
 	}
 }
